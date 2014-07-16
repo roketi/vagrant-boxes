64e242b3b6dc88473bba6b03d6d8d6a9e64d5378
Andri Steiner <asteiner@snowflake.ch>
2014-07-16 18:28:15 +0200

    Panel Module: Install nginx, GIT Clone Panel

--- modules/roketi/manifests/panel.pp
+++ modules/roketi/manifests/panel.pp
@@ -1,7 +1,33 @@
 
+# TODO: make Configurable
+
 class roketi::panel {
 
 	notify { "Hi! I'm the Roketi-Panel Server.": }
+
+	# Clone Panel
+	vcsrepo { '/var/www/panel':
+		ensure   => present,
+		provider => git,
+		source   => 'https://github.com/roketi/panel.git',
+	}
+
+	# include nginx
+	class { 'nginx':
+		vhost_purge => true,
+		confd_purge => true,
+	}
+
+	# vHost for Panel
+	nginx::resource::vhost { $fqdn:
+		www_root         => '/var/www/panel',
+	}
+
+	# PHP
+	# TODO: Install PHP, create FPM Pool and appropriate nginx Configuration
+
+	# Database
+	# TODO: Create roketi Database
 
 }
 
